9b52bfe191451db7421ef491a4ebc42b7fa3a522
peter <peter_rolfsen@hotmail.com>
2018-01-31 15:57:39 +0100

    no message

--- scrips/myJQ.js
+++ scrips/myJQ.js
@@ -1,11 +1,15 @@
 $(document).ready(function () {
 /*
 * function that adds class addon to the child of .img-responsive,  which is column-text when hovering over .img-responsive(the image)
+
+It also looks for the class "hidden-column-text", by html-tree. Therefore reffering to the children of the img-responsive. This is so the code will only execute on the image that is hovered over.
 */
     $(".img-responsive").hover(function (){
        $(this.children[0]).addClass("addon");
+       $(this.children[0].children[2]).css({"display":"block"});
     }, function(){
-        $(this.children[0]).removeClass("addon");
+       $(this.children[0]).removeClass("addon");
+       $(this.children[0].children[2]).css({"display":"none"});
     }
 );
     
