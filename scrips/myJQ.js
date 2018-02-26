acdf17be62fa45bc66438bb63fe36201c2f79615
peter <peter_rolfsen@hotmail.com>
2018-02-26 18:23:41 +0100

    - Gjorde alle anbefalinger slik bestilt

--- scrips/myJQ.js
+++ scrips/myJQ.js
@@ -5,11 +5,14 @@ $(document).ready(function () {
 It also looks for the class "hidden-column-text", by html-tree. Therefore reffering to the children of the img-responsive. This is so the code will only execute on the image that is hovered over.
 */
     $(".img-responsive").hover(function (){
-       $(this.children[0]).addClass("addon");
-       $(this.children[0].children[2]).css({"display":"block"});
+       $(this.children[0]).addClass("colorlayer2");
+       $(this.children[0].children[0]).css({"display": "none"});
+        $(this.children[0].children[1]).css({"display": "block"});
+    
     }, function(){
-       $(this.children[0]).removeClass("addon");
-       $(this.children[0].children[2]).css({"display":"none"});
+       $(this.children[0].children[0]).css({"display": "block"});
+        $(this.children[0].children[1]).css({"display": "none"});
+       $(this.children[0]).removeClass("colorlayer2");
     }
 );
     
