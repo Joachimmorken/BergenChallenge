8bb613d42ad5af24a4aff03e04ca3f4b2838240f
peter <peter_rolfsen@hotmail.com>
2018-02-28 21:18:28 +0100

    Tok bort footeren, fuck den. La til instafeed, ikke ferdig stylet.

---
+++ templates/assets/scrips/custom.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+
+
+      var userFeed = new Instafeed({
+        get: 'user',
+        userId: '176595535',
+        limit: 5,
+        resolution: 'standard_resolution',
+        accessToken: '176595535.1677ed0.0a71c932a6da4950b8c3f48fe1cffa52',
+        sortBy: 'most-recent',
+        template: '<div class="col-lg-2 instaimg"><a href="{{image}}" title="{{caption}}" target="_blank"><img src="{{image}}" alt="{{caption}}" class="img-fluid"/></a></div>',
+    });
+
+
+    userFeed.run();
+
+    
+    // This will create a single gallery from all elements that have class "gallery-item"
+
+
+});
