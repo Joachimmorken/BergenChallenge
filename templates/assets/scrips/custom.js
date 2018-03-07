b355a5ed7ec3ec09d0aa19811ef157de1a0f5241
peter <peter_rolfsen@hotmail.com>
2018-03-07 21:19:01 +0100

    Instagram er Nå SLBC sin, går nå ann å trykke på og bla igjennom de

--- templates/assets/scrips/custom.js
+++ templates/assets/scrips/custom.js
@@ -2,10 +2,10 @@ $(document).ready(function() {
     
               var userFeed = new Instafeed({
         get: 'user',
-        userId: '176595535',
+        userId: '422974041',
         limit:5,
         resolution: 'standard_resolution',
-        accessToken: '176595535.1677ed0.0a71c932a6da4950b8c3f48fe1cffa52',
+        accessToken: '422974041.1677ed0.a92d7f1e4279436fa267baed53abe353',
         sortBy: 'most-recent',
         template: '<div class="instaimg"><a href="{{image}}" title="{{caption}}" target="_blank"><img src="{{image}}" alt="{{caption}}" class="img-fluid"/></a></div>',
     });
@@ -14,7 +14,14 @@ $(document).ready(function() {
   
 
 
-
+// This will create a single gallery from all elements that have class "gallery-item"
+$('.gallery').magnificPopup({
+  type: 'image',
+    delegate: 'a',
+  gallery:{
+    enabled:true
+  }
+});
 
 
   
